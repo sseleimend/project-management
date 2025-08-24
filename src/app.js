e2524abdbb433158c73da697e5b66b89fcd2b6e4
Victor Seleimend <sseleimend@gmail.com>
2025-08-24 18:49:53 -0300

    refactor(app): streamline error handling and remove production HTTPS check

--- src/app.js
+++ src/app.js
@@ -1,16 +1,11 @@
 import { rateLimitConfig, corsConfig } from "./config/security.js";
-
 import secureCookies from "./middlewares/secureCookies.js";
-import errorHandler, { AppError } from "./middlewares/errorHandler.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import { ApiError } from "./utils/ApiError.js";
 import protectSensitiveRoutes from "./middlewares/security.js";
-
 import authRoutes from "./routes/auth.js";
 
 const app = express();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(protectSensitiveRoutes.ensureHttps);
-}
 
 app.use(express.json({ limit: "16kb" }));
 
@@ -32,7 +27,7 @@ app.use(cors(corsConfig));
 app.use("/api/auth", authRoutes);
 
 app.all(/.*/, (req, res, next) => {
-  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
+  next(new ApiError(`Cannot find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(errorHandler);

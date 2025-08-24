a65cc1db67015dd1a60d42e4017d62c9d2b2955b
Victor Seleimend <sseleimend@gmail.com>
2025-08-24 18:31:16 -0300

    refactor(cookie): remove unnecessary comments and streamline code

--- src/utils/cookieOptions.js
+++ src/utils/cookieOptions.js
@@ -1,11 +1,5 @@
-/**
- * Secure cookie configuration options
- * These settings help protect cookies from various attacks
- */
+import { cookieConfig } from "../config/security.js";
 
-import { cookieConfig } from '../config/security.js';
-
-// Use the centralized cookie configuration
 const cookieOptions = cookieConfig;
 
 export default cookieOptions;

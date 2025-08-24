9cb30ebcd74f3d1af61e650164e65a7c299cfc1c
Victor Seleimend <sseleimend@gmail.com>
2025-08-24 18:30:53 -0300

    refactor(app): streamline middleware imports and enhance security configurations

--- src/app.js
+++ src/app.js
@@ -1,38 +1,25 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import hpp from "hpp";
-import rateLimit from "express-rate-limit";
-import xss from "xss-clean";
-import mongoSanitize from "express-mongo-sanitize";
-import cookieParser from "cookie-parser";
+import { rateLimitConfig, corsConfig } from "./config/security.js";
 
 import secureCookies from "./middlewares/secureCookies.js";
-import { rateLimitConfig, cspConfig, corsConfig } from "./config/security.js";
-import authRoutes from "./routes/auth.js";
 import errorHandler, { AppError } from "./middlewares/errorHandler.js";
+import protectSensitiveRoutes from "./middlewares/security.js";
+
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 
-// Middlewares
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+if (process.env.NODE_ENV === "production") {
+  app.use(protectSensitiveRoutes.ensureHttps);
+}
 
-// Security
+app.use(express.json({ limit: "16kb" }));
+
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(secureCookies);
-
-app.use(helmet.contentSecurityPolicy(cspConfig));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.frameguard({ action: "deny" }));
-app.use(helmet.dnsPrefetchControl({ allow: false }));
-app.use(helmet.referrerPolicy({ policy: "same-origin" }));
+app.use(helmet());
+app.use(hpp());
 app.use(mongoSanitize());
 app.use(xss());
-app.use(hpp());
 
 const standardLimiter = rateLimit(rateLimitConfig.standard);
 const authLimiter = rateLimit(rateLimitConfig.auth);

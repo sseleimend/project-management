6a22279b121a336f37e1e0cab2e7c507ebf74cfd
Victor Seleimend <sseleimend@gmail.com>
2025-08-24 19:04:19 -0300

    feat(constants): add user roles and task status constants

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const UserRoles = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "project_admin",
+  MEMBER: "member",
+};
+
+export const AvailableUserRoles = Object.values(UserRoles);
+
+export const TaskStatus = {
+  TODO: "todo",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+};
+
+export const AvailableTaskStatus = Object.values(TaskStatus);

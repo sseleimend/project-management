9bc9b7d4a440329eae86f54e45e7121ce5217ecd
Victor Seleimend <sseleimend@gmail.com>
2025-08-27 20:23:58 -0300

    fix(dependencies): update express-mongo-sanitize to @exortek version and adjust imports

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import hpp from "hpp";
-import mongoSanitize from "express-mongo-sanitize";
+import expressMongoSanitize from "@exortek/express-mongo-sanitize";
 import { xss } from "express-xss-sanitizer";
 import rateLimit from "express-rate-limit";
 import cors from "cors";
@@ -11,6 +11,7 @@ import cors from "cors";
 import { rateLimitConfig, corsConfig } from "./config/security.js";
 import { secureCookies } from "./middlewares/secureCookies.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import { responseFormatter } from "./middlewares/responseFormatter.js";
 import { ApiError } from "./utils/ApiError.js";
 import { logger } from "./utils/logger.js";
 import { authRoutes } from "./routes/auth.routes.js";
@@ -31,12 +32,15 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use(secureCookies);
 app.use(helmet());
+app.use(xss());
 app.use(cors(corsConfig));
 app.use(hpp());
-app.use(mongoSanitize());
-app.use(xss());
+app.use(expressMongoSanitize());
+app.param("id", expressMongoSanitize.paramSanitizeHandler());
+
+app.use(secureCookies);
+app.use(responseFormatter);
 
 const standardLimiter = rateLimit(rateLimitConfig.standard);
 const authLimiter = rateLimit(rateLimitConfig.auth);

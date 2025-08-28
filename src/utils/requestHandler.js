3725f7b85a1b121c0b2d437013524885a8e25ee8
Victor Seleimend <sseleimend@gmail.com>
2025-08-28 19:55:02 -0300

    feat(controller): implement requestHandler for standardized request handling

---
+++ src/utils/requestHandler.js
@@ -0,0 +1,38 @@
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
+
+import ApiResponse from "./ApiResponse.js";
+
+export function requestHandler(controllerFn) {
+  return async (req, res, next) => {
+    try {
+      const response = await controllerFn(req);
+
+      let statusCode;
+      let message;
+
+      if (!response) {
+        statusCode = StatusCodes.NO_CONTENT;
+        message = getReasonPhrase(statusCode);
+      }
+
+      statusCode = response?.statusCode;
+      message = response?.message;
+
+      if (!message || !statusCode) {
+        throw new Error("Invalid response");
+      }
+
+      res.status(statusCode).json(
+        new ApiResponse({
+          status: "success",
+          message,
+          data: response?.data,
+        }),
+      );
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default requestHandler;

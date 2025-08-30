b307631eb5fe33e2cf962bec45e80edab97fad53
Victor Seleimend <sseleimend@gmail.com>
2025-08-30 13:44:08 -0300

    feat(auth): add signup route to handle user registration

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
 
 import { protectSensitiveRoutes } from "../middlewares/security.js";
+import { requestHandler } from "../utils/requestHandler.js";
+import { signUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.use(protectSensitiveRoutes.ensureHttps);
 router.use(protectSensitiveRoutes.validateContentType);
+
+router.post("/signup", requestHandler(signUp));
 
 export const authRoutes = router;
 

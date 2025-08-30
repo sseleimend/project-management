bddd9d72529fba49d200b385acac724b1d37421c
Victor Seleimend <sseleimend@gmail.com>
2025-08-29 22:40:38 -0300

    feat(user): add bcrypt for password hashing in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -73,6 +74,14 @@ const userSchema = new mongoose.Schema(
     versionKey: false,
   },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
 

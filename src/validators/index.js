55272d9823bc71fff854e9c774da08c2d145b032
Victor Seleimend <sseleimend@gmail.com>
2025-08-30 15:54:36 -0300

    feat(validators): add login validation for user authentication

--- src/validators/index.js
+++ src/validators/index.js
@@ -21,4 +21,19 @@ export const signUpValidator = [
     .withMessage("Password must be at least 6 characters long"),
 ];
 
-export default { signUpValidator };
+export const loginValidator = [
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email address"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
+
+export default { signUpValidator, loginValidator };
